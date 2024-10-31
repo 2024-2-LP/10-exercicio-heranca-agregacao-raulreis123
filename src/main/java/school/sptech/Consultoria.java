2a5b0bc10b826a3a74c32d6a057ebd02712248da
raulreis123 <raulgomesreis@gmail.com>
2024-10-31 13:27:17 -0300

    Atividade V1

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,39 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vagas;
+    List<Desenvolvedor> desenvolvedores = new ArrayList<>();
+
+    public Boolean contratar(Desenvolvedor desenvolvedor){
+        if(vagas > desenvolvedores.size()){
+            desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor){
+        if(desenvolvedor.isFullstack()){
+            desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Double getTotalSalarios(){
+        Double salario = 0.0;
+
+        for (Desenvolvedor dev : desenvolvedores){
+            salario += dev.calcularSalario();
+        }
+
+        return salario;
+    }
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
@@ -1,4 +1,38 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorMobile {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorMobile extends Desenvolvedor {
+    private String plataforma;
+    private String linguagem;
+    private Integer horasPrototipacao;
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public void setLinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+
+    public Integer getHorasPrototipacao() {
+        return horasPrototipacao;
+    }
+
+    public void setHorasPrototipacao(Integer horasProtipacao) {
+        this.horasPrototipacao = horasProtipacao;
+    }
+
+    @Override
+    public Double calcularSalario(){
+        return (qtdHoras * valorHora) + this.horasPrototipacao * 200;
+    }
 }
